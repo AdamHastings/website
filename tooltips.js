fedfb000051b02ae5777e1fbc1e8097ef8e0460a
adamhastings <itsadamhastings@gmail.com>
2023-08-18 10:54:10 -0400

    some uncommitted mountain stuff

--- tooltips.js
+++ tooltips.js
@@ -1,72 +1,25 @@
-// // function loadJSON() {
-// //   fetch('peaks.json')
-// //   .then(response => response.json())
-// //   .then(data => {
-// //     // Do something with the JSON data
-// //     peaks = data;
-// //   })
-// //   .catch(error => {
-// //     // Handle any errors that occur during the fetch
-// //     console.error('Error:', error);
-// //   });
-// // }
 
-// var peaks = '{}';
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   // Your JavaScript code here
-  
-//   // fetch('peaks.json')
-//   // .then(response => response.json())
-//   // .then(data => {
-//   //   // Do something with the JSON data
-//   //   peaks = data;
-//   // })
-//   // .catch(error => {
-//   //   // Handle any errors that occur during the fetch
-//   //   console.error('Error:', error);
-//   // });
-
-//   const response = fetch("peaks.json");
-//   const jsonData = response.json();
-//   console.log(jsonData);
-
-//   // console.log(peaks);
-//   console.log('JSON loaded!');
-// });
-
-// const peaks = fetch("peaks.json")
-//      .then(response => {
-//          if (!response.ok) {
-//              throw new Error(`HTTP error ${response.status}`);
-//          }
-//          return response.json();
-//      });
-
-// console.log(peaks)
-// console.log(peaks['Mount Rainier'])
 var peaks = '';
-console.log(peaks);
 var request = new XMLHttpRequest();
 request.open('GET', 'peaks.json', true);
 request.onreadystatechange = function() {
   if (request.readyState === 4 && request.status === 200) {
     peaks = JSON.parse(request.responseText);
     // Do something with the JSON data
-    console.log(peaks);
   }
 };
 request.send();
-console.log(peaks);
 
 function showTooltip(evt, text) {
   let tooltip = document.getElementById("tooltip");
   let map = document.getElementById("container");
   let name = document.getElementById("peakbox_header");
   name.innerHTML = text;
+  name.style.padding = "10px";
+
 
   if (text == "Mount Washington") {
-    tooltip.style.left = evt.pageX - map.offsetLeft - 550 + 'px';
+    tooltip.style.left = evt.pageX - map.offsetLeft - 450 + 'px';
     tooltip.style.top = evt.pageY - map.offsetTop - 60 + 'px';
   } else if (text == "Mount Mitchell") {
     tooltip.style.left = evt.pageX - map.offsetLeft - 325 + 'px';
@@ -76,12 +29,7 @@ function showTooltip(evt, text) {
     tooltip.style.top = evt.pageY - map.offsetTop - 20 + 'px';
   }
 
-
-  // console.log(text);
-  // console.log(peaks[text])
-
-  peak = peaks[text]
-  
+  peak = peaks[text]  
   
   document.getElementById("box-elevation").innerHTML = peak.elevation;
   document.getElementById("box-prominence").innerHTML = peak.prominence;
@@ -101,7 +49,11 @@ function showTooltip(evt, text) {
 
       // Add some text to the new cells:
       cell1.innerHTML = "Route:";
+      cell1.style.textAlign = "right";
+      cell1.style.verticalAlign = "top";
+      cell1.style.paddingRight = "10px";
       cell2.innerHTML = peak.route;
+      // cell2.style.textAlign = "justify";
     }
 
     let descrow = mytable.insertRow(-1);
@@ -112,63 +64,14 @@ function showTooltip(evt, text) {
       let cell2 = descrow.insertCell(1);
 
       // Add some text to the new cells:
-      cell1.innerHTML = "Description:";
+      cell1.innerHTML = "Note:";
+      cell1.style.textAlign = "right";
+      cell1.style.verticalAlign = "top";
+      cell1.style.paddingRight = "10px";
       cell2.innerHTML = peak.description;
+      // cell2.style.textAlign = "justify";
     }
   }
-
-
-  // var xmlhttps = new XMLHttpRequest();
-  // xmlhttps.onreadystatechange = function () {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     var response = JSON.parse(this.responseText);
-  //     console.log(response);
-  //     console.log(response.elevation);
-  //     document.getElementById("box-elevation").innerHTML = response.elevation;
-  //     document.getElementById("box-prominence").innerHTML = response.prominence;
-  //     document.getElementById("box-range").innerHTML = response.range;
-
-  //     let mytable = document.getElementById("tooltip-table");
-
-  //     if (response.route != "" && response.description != "" && mytable.rows.length == 3) {
-  //       // Find a <table> element with id="myTable":
-
-  //       // Create an empty <tr> element and add it to the 1st position of the table:
-  //       let routerow = mytable.insertRow(-1);
-
-  //       {
-  //         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-  //         let cell1 = routerow.insertCell(0);
-  //         let cell2 = routerow.insertCell(1);
-
-  //         // Add some text to the new cells:
-  //         cell1.innerHTML = "Route:";
-  //         cell2.innerHTML = response.route;
-  //       }
-
-  //       let descrow = mytable.insertRow(-1);
-
-  //       {
-  //         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-  //         let cell1 = descrow.insertCell(0);
-  //         let cell2 = descrow.insertCell(1);
-
-  //         // Add some text to the new cells:
-  //         cell1.innerHTML = "Description:";
-  //         cell2.innerHTML = response.description;
-  //       }
-
-  //     }
-  //   }
-  // }
-  // var getInfoScript = "info.php";
-  // var url = getInfoScript.concat("?", text);
-  // url = url.replace(/\s/g, '+')
-  // //console.log(url);
-
-
-  // xmlhttps.open("GET", url);
-  // xmlhttps.send();
 
   tooltip.style.display = "block";
 }
