623281498dc4f0aded2b64d5ea0b032dd328bb84
adamhastings <itsadamhastings@gmail.com>
2024-07-11 16:21:58 -0400

    typo fix

--- process_password.php
+++ process_password.php
@@ -13,8 +13,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         Hey, you made it. Congrats on solving the two easy (too easy?) puzzles. 
                     </p>
                     <p>
-                        I upload my monthly playlists to Spotify profile <a href="https://open.spotify.com/user/1267021586" target="_blank" rel="noopener noreferrer" class="link-class">here</a>.
-                        For the compressed version, here\'s a playlist of 100 songs I often have on repeat (one song per artist, sorted alphabetically). 
+                        I upload my monthly playlists to my Spotify profile <a href="https://open.spotify.com/user/1267021586" target="_blank" rel="noopener noreferrer" class="link-class">here</a>.
+                        For the compressed version, below is a playlist of 100 songs I often have on repeat (one song per artist, sorted alphabetically). 
                     </p>
                     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/68mmKV6b3gUiuLEU5r3hcb?utm_source=generator" width="100%" height="1000" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
